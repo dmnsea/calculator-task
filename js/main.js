0ddc79167a44b209bec58ec4b28bb60ee773e9b4
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2025-02-06 15:07:53 +0300

    Заготовка под модальные диалоги
    Для отображения информации о калькуляторе и истории вычислений

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import { loadTheme } from "./theme.js";
 import { setButtonHandlers } from "./buttons.js";
 import { addKeyBindings } from "./bindings.js";
+import { enableModalDialogs } from "./modals.js";
 
 addEventListener("load", () => {
   loadTheme();
   setButtonHandlers();
   addKeyBindings();
+  enableModalDialogs();
 });
